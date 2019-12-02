4184da85363bb0d3279f77f7f69018a1f2edb4f0
Louis Weyland <Louis@MacBook-Pro-de-Louis.local>
2019-12-02 22:50:11 +0100

    Added the classes and a ploting function

--- code/function.py
+++ code/function.py
@@ -1,11 +1,56 @@
 import numpy as np
+import matplotlib.pyplot as plt 
+import random 
+
+# =============================================================================
+
 
 # calculate the energy level of one particle
 def calc_energy_1p(coordinates, particlenumber, list_particles):
     E = 0
     for i in range(len(list_particles)):
-        distance = np.sqrt((coordinates[0] - list_particles[i].x) ** 2 + (coordinates[1] - list_particles[i].y) ** 2)
+        distance = np.sqrt((coordinates[0] - list_particles[i].x) ** 2 + \
+                           (coordinates[1] - list_particles[i].y) ** 2)
         if distance != 0:
             E += 1 / distance
 
     return E
+
+# =============================================================================
+
+# Class to create the different objects 
+
+class particle:
+    
+    def __init__(self):
+        self.x=random.uniform(-1,1)
+        # To make sure that the points are within the circle
+        self.y=random.uniform(-np.sqrt(1-self.x**2),np.sqrt(1-self.x**2))
+     
+     
+class circle:
+    
+    def __init__(self):
+        self.theta=np.linspace(0,2*np.pi,100)
+        self.radius=1
+        self.x=self.radius*np.cos(self.theta)
+        self.y=self.radius*np.sin(self.theta)
+   
+
+# =============================================================================    
+
+#  Plot the circle with the points in it 
+    
+def plot_circle(particles,circle):
+    
+    x,y = zip(*[((i.x),float(i.y)) for i in particles])
+
+    plt.axis("equal")
+    plt.scatter(x,y,color='k')
+    plt.plot(circle.x,circle.y,'k')
+    plt.xlim(-1,1)
+    plt.ylim(-1,1)
+    plt.show()
+    
+
+# =============================================================================
--- code/main.py
+++ code/main.py
@@ -1,1 +1,17 @@
 '''This code was implemented by Louis Weyland & Robin van den Berg'''
+
+from function import *
+
+
+# create particles
+number_of_particles=30
+particles=[particle() for i in range(number_of_particles)]
+
+#create circle
+circle=circle()
+
+for i in range(0,number_of_particles):
+   print("x coordinates: ",particles[i].x,"Y coordinates :", particles[i].y)
+
+plot_circle(particles,circle)
+    
