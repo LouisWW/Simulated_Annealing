4cd824977f3b654f6886554be2c44164357939a6
robinvandenberg73451 <robinvandenberg1998@gmail.com>
2019-12-02 19:52:24 +0100

    First bit of the configuration function with a start on the initialisation function for the global variables

---
+++ code/change_configuration.py
@@ -0,0 +1,18 @@
+###This file takes a list of particles representating one configuration and produces another
+###one
+
+
+def change_config(list_particles):
+    N = len(list_particles)
+
+    # change the location of every particle
+    for i in range(N):
+        # move particle i
+        list_particles[i].x =
+        list_particles[i].y =
+
+        # check whether it is accepted
+
+
+
+
---
+++ code/global_variables.py
@@ -0,0 +1,5 @@
+# this file defines the function that will initialise the global variables
+
+def init_global(r):
+
+    global circle(r)  # something like this to initialise the circle were using
