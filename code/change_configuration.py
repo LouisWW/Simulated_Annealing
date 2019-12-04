b584797611827c532c6653d6bbf8eba0a13caa55
robinvandenberg73451 <robinvandenberg1998@gmail.com>
2019-12-04 10:00:42 +0100

    modifications to change configuratoins

--- code/change_configuration.py
+++ code/change_configuration.py
@@ -19,32 +19,47 @@ def change_config(list_particles, T):
     for i in range(N):
 
         # move particle i
-        temp_x = list_particles[i].x + np.random.choice([-1, 1]) * np.random.exponential(1/av_stepsize)
-        temp_y = list_particles[i].y + np.random.choice([-1, 1]) * np.random.exponential(1/av_stepsize)
+        temp_x = list_particles[i].x + np.random.choice([-1, 1]) * np.random.exponential(av_stepsize)
+        temp_y = list_particles[i].y + np.random.choice([-1, 1]) * np.random.exponential(av_stepsize)
 
         # check whether point falls within sphere
         if temp_y**2 + temp_x**2 <= r_squared:
 
             # compare the energy of the old configuration to the new one
-            dE = calc_energy_1p([temp_x, temp_y], i, list_particles) - calc_energy_1p([temp_x, temp_y], i, list_particles)
-
+            dE = calc_energy_1p([temp_x, temp_y], i, list_particles) - calc_energy_1p([list_particles[i].x, list_particles[i].y], i, list_particles)
+            print("The energy change: ", dE)
             # if energy is lower, we accept the new state
             if dE < 0:
-                list_particles.x = temp_x
-                list_particles.y = temp_y
+                print("energy is lower, lets move")
+                list_particles[i].x = temp_x
+                list_particles[i].y = temp_y
 
             # if energy is higher, but we still accept the value
-            if dE > 0 and accept_config(dE, T) == True:
-                list_particles.x = temp_x
-                list_particles.y = temp_y
+            if (dE > 0) and (accept_config(dE, T) == True):
+                print("we moved the particle because of chance")
+                list_particles[i].x = temp_x
+                list_particles[i].y = temp_y
+
+    return list_particles
+
 
 # mock function to test the change config function
 def accept_config(dE, T):
-    if np.random.rand() > 0.5:
+    p_boltzmann = np.exp(dE)/T
+    print(p_boltzmann)
+    if np.random.rand() > p_boltzmann:
         return True
     else:
         return False
 
 list_particles = []
 for i in range(3):
-    list_particles.appedn
+    list_particles.append(Particle())
+
+circle=Circle(r=1)
+plot_circle(list_particles,circle)
+
+list_particles = change_config(list_particles, 100)
+
+plot_circle(list_particles, circle)
+
