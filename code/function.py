6838c24b9db82190255eab0f7ab8106effaef09f
Louis Weyland <Louis@wcw-stud-145-109-36-206.wireless.uva.nl>
2019-12-04 10:00:01 +0100

    Newest function and main

--- code/function.py
+++ code/function.py
@@ -5,16 +5,38 @@ import random
 # =============================================================================
 
 # calculate the energy level of one particle
-def calc_energy_1p(coordinates, particlenumber, list_particles):
+def calc_energy_1p(coordinates, particle_number, list_particles):
     E = 0
     for i in range(len(list_particles)):
-        distance = np.sqrt((coordinates[0] - list_particles[i].x) ** 2 + \
+        
+        if i != particle_number:
+            distance = np.sqrt((coordinates[0] - list_particles[i].x) ** 2 + \
                            (coordinates[1] - list_particles[i].y) ** 2)
-        if distance != 0:
+           
             E += 1 / distance
 
     return E
 
+
+
+def total_energy(list_particles):
+    
+    total_energy=0
+    
+    for i in range(0,len(list_particles)):
+        
+        for k in range(i+1,len(list_particles)):
+            
+            distance= np.sqrt((list_particles[i].x - list_particles[k].x) ** 2 + \
+                           (list_particles[i].x - list_particles[k].y) ** 2)
+          
+           
+            total_energy+=distance
+            
+    
+    return total_energy
+            
+            
 # =============================================================================
 
 # Class to create the different objects
@@ -37,9 +59,9 @@ class Circle:
 
 #  Plot the circle with the points in it 
     
-def plot_circle(particles,circle):
+def plot_circle(list_particles,circle):
     
-    x,y = zip(*[((i.x),float(i.y)) for i in particles])
+    x,y = zip(*[((i.x),float(i.y)) for i in list_particles])
 
     plt.axis("equal")
     plt.scatter(x,y,color='k')
--- code/main.py
+++ code/main.py
@@ -4,16 +4,22 @@ from function import *
 
 
 # create particles
-number_of_particles=30
-particles=[Particle() for i in range(number_of_particles)]
+number_of_particles=5
+list_particles=[Particle() for i in range(number_of_particles)]
 
 #create circle
 circle=Circle(r=1)
 
+#Can also be deleted
 for i in range(0,number_of_particles):
-   print("x coordinates: ",particles[i].x,"Y coordinates :", particles[i].y)
+   print("x coordinates: ",list_particles[i].x,"Y coordinates :", list_particles[i].y)
 
-plot_circle(particles,circle)
+#plot_circle(list_particles,circle)
 
+
+total_energy=total_energy(list_particles)
+
+# Can be deleted
+print("total energy :",total_energy)
 
 
